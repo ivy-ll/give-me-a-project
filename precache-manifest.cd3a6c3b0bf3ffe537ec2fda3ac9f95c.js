92b41bf909b28bbdfcbafff744b525b5bf239af9
cclin130 <cheng.lin103@gmail.com>
2020-03-29 13:11:28 -0400

    Updates

---
+++ precache-manifest.cd3a6c3b0bf3ffe537ec2fda3ac9f95c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65c4f7b931e2c41a50894df8a8a8219",
+    "url": "/give-me-a-project/index.html"
+  },
+  {
+    "revision": "d64cb4a7aff9277d4062",
+    "url": "/give-me-a-project/static/css/main.79596b6a.chunk.css"
+  },
+  {
+    "revision": "2e36e36a7190f25e9856",
+    "url": "/give-me-a-project/static/js/2.bae8fc53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/give-me-a-project/static/js/2.bae8fc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64cb4a7aff9277d4062",
+    "url": "/give-me-a-project/static/js/main.fc47c975.chunk.js"
+  },
+  {
+    "revision": "a1707a87fa621ac55c42",
+    "url": "/give-me-a-project/static/js/runtime-main.be8110bd.js"
+  },
+  {
+    "revision": "9c65d061fbdf45b56b43cf3be9ae964a",
+    "url": "/give-me-a-project/static/media/logo.9c65d061.svg"
+  }
+]);
