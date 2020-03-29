7846c7dc2db695b8578bdbfdb424e0cfbddc89f3
cclin130 <cheng.lin103@gmail.com>
2020-03-29 01:52:20 -0400

    frontend almost done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # give-me-a-project
 web app to generate random beginner friendly open source projects
+
+To run, use the following commands in terminal:
+```
+npm install
+npm start
+```
--- src/App.js
+++ src/App.js
@@ -2,25 +2,89 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Button(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button
+      className='btn'
+      onClick={props.onClick}>
+      {props.val}
+    </button>
+  );
+}
+
+function Project(props){
+  let val = 'val';
+  let name = 'hello world';
+  let lang = 'Language: ' + 'java';
+  let link = 'https://google.ca';
+  let description = 'blah blah';
+
+  return (
+    <div class='project-panel'>
+      <a class='project-name'>{name}</a>
+      <a class='project-language'>{lang}</a>
+      <a href={link}>Repository</a>
+      <a class='project-desc'>{description}</a>
     </div>
   );
+}
+
+function Selection(props) {
+  if (props.buttonClicked) {
+    return (
+      <div>
+        <Project />
+        <Button
+          val='Change project'
+          onClick={() => props.onClick()}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Button 
+          val='Give me a project'
+          onClick={() => props.onClick()}
+        />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonClicked: false,
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      buttonClicked: true,
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Get a random beginner-friendly open source project!
+          </p>
+          <Selection 
+            buttonClicked={this.state.buttonClicked}
+            onClick={() => this.handleClick()}
+          />
+        </header>
+        <footer>
+          Site built with <a href="https://create-react-app.dev/">Create React App</a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
